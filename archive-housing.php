51b992efb26bed5c85ec70ba003fceffb19953b6
Matthieu Mota <matthieumota@gmail.com>
2019-12-20 15:13:57 +0100

    Card integration housing

--- archive-housing.php
+++ archive-housing.php
@@ -8,20 +8,33 @@ get_header(); ?>
     			<div class="row">
 	    			<?php while (have_posts()) : the_post(); ?>
 	    				<div class="col-lg-3">
+	    					<?php
+    							// Récupère l'url de l'image
+    							$image_url = wp_get_attachment_image_src(
+    								get_post_thumbnail_id( $post->ID ), 'large'
+    							);
+    							// On veut le chemin de l'image
+    							$image_url = $image_url[0];
+    						?>
+						    <img class="card-img-top" src="<?= $image_url; ?>" alt="<?php the_title(); ?>">
 	    					<div class="card">
-	    						<?php
-	    							// Récupère l'url de l'image
-	    							$image_url = wp_get_attachment_image_src(
-	    								get_post_thumbnail_id( $post->ID ), 'large'
-	    							);
-	    							// On veut le chemin de l'image
-	    							$image_url = $image_url[0];
-	    						?>
-							    <img class="card-img-top" src="<?= $image_url; ?>" alt="<?php the_title(); ?>">
-							    <?php // the_post_thumbnail( [100, 100] ); ?>
+							    <?php // the_post_thumbnail( [100, 100] ); 
+							    	$surface = get_post_meta( $post->ID, '_surface', true );
+							    	$price = get_post_meta( $post->ID, '_price', true );
+
+							    	if (empty($surface)) $surface = 1;
+							    	if (empty($price)) $price = 0;
+
+							    	$priceByM = $price / $surface;
+							    ?>
 							    <div class="card-body">
-							        <h5 class="card-title"><?php the_title(); ?></h5>
-							        <p class="card-text"><?php the_content(); ?></p>
+							    	<span class="text-muted"><?= $surface; ?> m²</span>
+							        <h5 class="card-title mb-0"><?php the_title(); ?></h5>
+							        <p class="mb-3 card-price">
+							        	<?= number_format($price, 2, ',', ' '); ?> €
+							        	(<?= number_format($priceByM, 2, ',', ' '); ?> € le m²)
+							        </p>
+							        <div class="card-text"><?php the_content(); ?></div>
 							        <a href="<?php the_permalink(); ?>" class="btn btn-primary">
 							        	Voir l'annonce
 							        </a>
--- taxonomy-city.php
+++ taxonomy-city.php
@@ -21,7 +21,7 @@ get_header(); ?>
 							    <?php // the_post_thumbnail( [100, 100] ); ?>
 							    <div class="card-body">
 							        <h5 class="card-title"><?php the_title(); ?></h5>
-							        <p class="card-text"><?php the_content(); ?></p>
+							        <div class="card-text"><?php the_content(); ?></div>
 							        <a href="<?php the_permalink(); ?>" class="btn btn-primary">
 							        	Voir l'annonce
 							        </a>
